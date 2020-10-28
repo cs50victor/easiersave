017532a7a73e1ac5c2fb7f7ba68cb96d7da11219
Victor (cs50Victor) <52110451+cs50victor@users.noreply.github.com>
2020-10-28 11:56:42 -0400

    removed share added about page

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,20 +3,6 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Roboto`,
-            variants: [`400`,`700`]
-          },
-          {
-            family: `Bungee`
-          }
-        ],
-      },
-    },
-    {
       resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Container, Navbar} from "react-bootstrap";
-import {FaTwitter} from "react-icons/fa"
+import {Link} from "gatsby";
 //import {TwitterShareButton} from "react-twitter-embed"
 import "../styles/components/_layout.scss";
 
@@ -17,22 +17,12 @@ const Layout =({children})=>{
                             </a>
                         </div>
                         <div>
-                            <a
-                            href="http://twitter.com/intent/tweet?text=If%20you%20are%20having%20a%20hard%20time%20downloading%20videos%2C
-                            %20use%20easiersave%2Ecom%20to%20save%20videos
-                            from%20Twitter%2C%20Instagram%2C%20TikTok%20and%20other%20websites
-                            to%20your device%2E%20Save%20it%20to%20your%20home%20screen%20to%20speed
-                            up%20future%20video%20downloads%2E
-                            %20%40easiersave" 
-                            className="twitter-mention-button"
-                            >
+                            <Link to="/about/" className="twitter-mention-button">
                                 <strong>
-                                    Share
+                                    About
                                     {" "}
-                                    <FaTwitter/>
-                                </strong>
-                            
-                            </a>
+                                </strong> 
+                           </Link>
                         </div>
                     </Navbar>
                     <hr/>
