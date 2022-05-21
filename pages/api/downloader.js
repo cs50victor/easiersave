4d49ccc0547d158f261466bf6891c0e616c41ad2
Victor A <52110451+cs50victor@users.noreply.github.com>
2022-05-21 14:12:04 -0400

    serverless function update

--- pages/api/downloader.js
+++ pages/api/downloader.js
@@ -1,4 +1,4 @@
-const youtubedl = require("youtube-dl")
+const ytdl = require("ytdl-core")
 const urlChecker = new RegExp("^(http|https)://")
 
 // http://localhost:8000/api/downloader?videoUrl=https://www.youtube.com/watch?v=JGwWNGJdvx8
@@ -12,20 +12,23 @@ export default async function downloader(req, res) {
   const { videoUrl } = req.query
 
   if (urlChecker.test(videoUrl)) {
-      
-      await youtubedl.getInfo(videoUrl, (error, info)=>{
-        if (error){
-          return res.status(404).json({
-            error: "Unable to download video. Please try a different url or try again later.",
-          })
-        } 
-        return res.status(200).json({
-          url: info.url,
-          thumbnail: info.thumbnail,
-          title: info.title
-        })
-      })
+    try{
 
+      let info = await ytdl.getInfo(videoUrl)
+      
+      const url = info.formats.filter((video)=>video.hasVideo && video.hasAudio).sort((a,b) => a.width > b.width).pop().url
+      const thumbnail = info.videoDetails.thumbnails.sort((a,b) => a.width > b.width).pop().url
+      const title = info.videoDetails.title
+      
+      return res.status(200).json({url,thumbnail,title})
+    }
+    catch (error){
+      console.log(error)
+      return res.status(404).json({
+        error:
+          "Unable to download video. Please try a different Youtube URL.",
+      })
+    }
   } else {
     return res.status(400).json({
       error: "Invalid Url. Make sure your link starts with 'http:// or https://'",
